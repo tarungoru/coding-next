7d628f4435756846c1322a474afbe8ef80f8e9ba
tarun <107760613+tarungoru@users.noreply.github.com>
2023-06-16 22:37:14 +0530

    Create Longest Substring Without Repeating Characters.py

---
+++ main/arrays/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,28 @@
+Input: s = "abcabcbb"
+Output: 3
+  
+# "optimised solution" using the array
+res=[]
+c=0
+ct=0
+for i in range(len(s)):
+  while res and (s[i] in res):
+    res.pop(0)
+    c-=1
+  res.append(s[i])
+  c+=1
+  ct=max(ct,c)
+  print(ct)
+  
+# optimised solution using the set
+
+res=set()
+c=0
+ct=0
+for i in range(len(s)):
+  while(s[i] in res):
+    res.remove(s[c])
+    c+=1
+  res.add(s[i])
+  ct=max(ct,i-c+1)
+print(ct)
