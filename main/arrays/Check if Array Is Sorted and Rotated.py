5fdc83b2c60b6f44e9d29bf4c6646cc479e90fd1
tarun <107760613+tarungoru@users.noreply.github.com>
2023-06-28 09:39:48 +0530

    Create  Check if Array Is Sorted and Rotated.py

---
+++ main/arrays/Check if Array Is Sorted and Rotated.py
@@ -0,0 +1,21 @@
+Input: nums = [3,4,5,1,2]
+Output: true
+
+# optimised code
+
+count=0
+index=0
+for i in range(len(nums)-1):
+  if nums[i]>nums[i+1]:
+    count+=1
+    index=i+1
+if count==0:
+  print(True)
+elif count==1:
+  arr=nums[index:]+nums[:index]
+  for i in range(len(arr)-1):
+    if arr[i+1]<arr[i]:
+      print(False)
+  print(False)
+else:
+  print(False)
